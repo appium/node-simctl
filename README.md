e32f335ba67e96f2e24e3d0c709fabcbca2fd98e
Jonathan Lipps <jlipps@gmail.com>
2014-12-04 15:21:35 -0800

    fix up tests and add readme

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 node-simctl
 ===========
 
-Node wrapper around Apple's simctl binary
+ES6/7 Node wrapper around Apple's simctl binary.
+
+```
+npm install node-simctl
+```
+
+See `lib/es6/specs.js` for examples of usage.
--- test/es6/specs.js
+++ test/es6/specs.js
@@ -1,7 +1,47 @@
 /* global it:true, describe:true*/
+import sms from 'source-map-support';
+sms.install();
+import 'traceur/bin/traceur-runtime';
 import 'mochawait';
 import 'should';
-import { deleteDevice, eraseDevice, getDevices } from '../../lib/es5/simctl.js';
+import { mapify } from 'es6-mapify';
+import _ from 'lodash';
+import { createDevice, deleteDevice, eraseDevice, getDevices } from '../../lib/es5/simctl.js';
 
-describe('deleteDevice', () => {
+describe('simctl', () => {
+  let randNum = parseInt(Math.random() * 100, 10);
+  let randName = `device${randNum}`;
+  let randDeviceUdid = null;
+  let validSdks = [];
+
+  it('should create a device', async () => {
+    let devices = await getDevices();
+    validSdks = _.keys(devices);
+    if (!validSdks.length) {
+      throw new Error("No valid SDKs");
+    }
+    for (let list of mapify(devices).values()) {
+      if (_.contains(_.pluck(list, 'name'), randName)) {
+        throw new Error(`Couldn't run test because device ${randName} ` +
+                        `already exists`);
+      }
+    }
+    await createDevice(randName, 'iPad Air', validSdks[0]);
+  });
+
+  it('should get devices', async () => {
+    let sdkDevices = await getDevices(validSdks[0]);
+    _.pluck(sdkDevices, 'name').should.containEql(randName);
+    randDeviceUdid = sdkDevices.filter((d) => d.name === randName)[0].udid;
+  });
+
+  it('should erase devices', async () => {
+    await eraseDevice(randDeviceUdid);
+  });
+
+  it('should delete devices', async () => {
+    await deleteDevice(randDeviceUdid);
+    let sdkDevices = await getDevices(validSdks[0]);
+    _.pluck(sdkDevices, 'name').should.not.containEql(randName);
+  });
 });
