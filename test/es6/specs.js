33c0acde25736341f310e4b83f76b64b8312c083
Jonathan Lipps <jlipps@gmail.com>
2014-12-10 10:20:07 -0800

    clean up callback mode and fix bad type assertion

--- test/es6/specs.js
+++ test/es6/specs.js
@@ -7,7 +7,7 @@ import 'mochawait';
 import should from 'should';
 import { mapify } from 'es6-mapify';
 import _ from 'lodash';
-import { createDevice, deleteDevice, eraseDevice, getDevices, cb } from '../../lib/es5/simctl.js';
+import { createDevice, deleteDevice, eraseDevice, getDevices } from '../../lib/es5/simctl.js';
 
 describe('simctl', () => {
   let randNum = parseInt(Math.random() * 100, 10);
@@ -34,14 +34,6 @@ describe('simctl', () => {
     let sdkDevices = await getDevices(validSdks[0]);
     _.pluck(sdkDevices, 'name').should.containEql(randName);
     randDeviceUdid = sdkDevices.filter((d) => d.name === randName)[0].udid;
-  });
-
-  regIt('should get devices in callback mode', done => {
-    cb.getDevices(validSdks[0], (err, sdkDevices) => {
-      should.not.exist(err);
-      _.pluck(sdkDevices, 'name').should.containEql(randName);
-      done();
-    });
   });
 
   it('should erase devices', async () => {
