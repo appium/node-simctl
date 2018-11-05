0efebd3bb1ac22dac68b9cfd0e0640b27a74549f
Mykola Mokhnach <nick+1@wire.com>
2018-11-05 17:15:05 +0100

    Remove async

--- test/simctl-e2e-specs.js
+++ test/simctl-e2e-specs.js
@@ -136,7 +136,7 @@ describe('simctl', function () {
       }
     });
 
-    describe('startBootMonitor', async function () {
+    describe('startBootMonitor', function () {
       it('should be fulfilled if the simulator is already booted', async function () {
         await startBootMonitor(udid).should.eventually.be.fulfilled;
       });
