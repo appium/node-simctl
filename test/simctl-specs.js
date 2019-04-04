3e68853666ba7525f657242e1e4e4625af6f1b92
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-04-04 14:59:37 +0900

    fix creating simulators for new format (#81)

--- test/simctl-specs.js
+++ test/simctl-specs.js
@@ -3,8 +3,8 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import * as TeenProcess from 'teen_process';
 import _ from 'lodash';
-import { getDevices } from '../lib/simctl';
-
+import { getDevices, createDevice } from '../lib/simctl';
+import * as xcode from 'appium-xcode';
 
 const devicePayloads = [
   [
@@ -29,8 +29,9 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('simctl', function () {
+  const execStub = sinon.stub(TeenProcess, 'exec');
+
   describe('getDevices', function () {
-    const execStub = sinon.stub(TeenProcess, 'exec');
     afterEach(function () {
       execStub.resetHistory();
     });
@@ -114,5 +115,94 @@ describe('simctl', function () {
         });
       });
     }
+  });
+
+  describe('#createDevice', function () {
+    const devicesPayload = devicePayloads[0][0];
+    const getCLTVersionStub = sinon.stub(xcode, 'getCommandLineToolsVersion');
+    const getXcodeVersionStub = sinon.stub(xcode, 'getVersion');
+    afterEach(function () {
+      execStub.resetHistory();
+      getCLTVersionStub.resetHistory();
+    });
+    after(function () {
+      execStub.reset();
+      getCLTVersionStub.reset();
+    });
+
+    it('should create iOS simulator by default', async function () {
+      execStub.onFirstCall().returns({stdout: 'com.apple.CoreSimulator.SimRuntime.iOS-12-1-1', stderr: ''})
+              .onSecondCall().returns({stdout: 'EE76EA77-E975-4198-9859-69DFF74252D2', stderr: ''})
+              .onThirdCall().returns(devicesPayload);
+      getCLTVersionStub.returns('10.2.0.0.1.1552586384');
+
+      const devices = await createDevice(
+        'name',
+        'iPhone 6 Plus',
+        '12.1.1',
+        { timeout: 20000 }
+      );
+      execStub.secondCall.args[1].should.eql([
+        'simctl', 'create', 'name', 'iPhone 6 Plus', 'com.apple.CoreSimulator.SimRuntime.iOS-12-1-1'
+      ]);
+      devices.should.eql('EE76EA77-E975-4198-9859-69DFF74252D2');
+    });
+
+    it('should create tvOS simulator', async function () {
+      execStub.onFirstCall().returns({stdout: 'com.apple.CoreSimulator.SimRuntime.tvOS-12-1', stderr: ''})
+              .onSecondCall().returns({stdout: 'FA628127-1D5C-45C3-9918-A47BF7E2AE14', stderr: ''})
+              .onThirdCall().returns(devicesPayload);
+      getCLTVersionStub.returns('10.2.0.0.1.1552586384');
+
+      const devices = await createDevice(
+        'name',
+        'Apple TV',
+        '12.1',
+        { timeout: 20000, platform: 'tvOS' }
+      );
+      execStub.secondCall.args[1].should.eql([
+        'simctl', 'create', 'name', 'Apple TV', 'com.apple.CoreSimulator.SimRuntime.tvOS-12-1'
+      ]);
+      devices.should.eql('FA628127-1D5C-45C3-9918-A47BF7E2AE14');
+    });
+
+    it('should create iOS simulator by default with lower command line tool but newer xcode version', async function () {
+      execStub.onFirstCall().returns({stdout: '12.1', stderr: ''})
+              .onSecondCall().returns({stdout: 'EE76EA77-E975-4198-9859-69DFF74252D2', stderr: ''})
+              .onCall(3).returns(devicesPayload);
+      getCLTVersionStub.returns('10.1.0.0.1.1552586384');
+      getXcodeVersionStub.returns('10.1');
+
+      const devices = await createDevice(
+        'name',
+        'iPhone 6 Plus',
+        '12.1',
+        { timeout: 20000 }
+      );
+      execStub.secondCall.args[1].should.eql([
+        'simctl', 'create', 'name', 'iPhone 6 Plus', '12.1'
+      ]);
+      devices.should.eql('EE76EA77-E975-4198-9859-69DFF74252D2');
+    });
+
+    it('should create iOS simulator by default with old format', async function () {
+      execStub.onFirstCall().returns({stdout: '12.1', stderr: ''})
+              .onSecondCall().returns({stdout: 'EE76EA77-E975-4198-9859-69DFF74252D2', stderr: ''})
+              .onCall(2).returns(devicesPayload);
+      getCLTVersionStub.returns('10.1.0.0.1.1552586384');
+      getXcodeVersionStub.returns('10.1');
+
+      const devices = await createDevice(
+        'name',
+        'iPhone 6 Plus',
+        '12.1',
+        { timeout: 20000 }
+      );
+      execStub.secondCall.args[1].should.eql([
+        'simctl', 'create', 'name', 'iPhone 6 Plus', '12.1'
+      ]);
+      devices.should.eql('EE76EA77-E975-4198-9859-69DFF74252D2');
+    });
+
   });
 });
